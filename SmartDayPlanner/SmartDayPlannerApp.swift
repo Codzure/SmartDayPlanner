a9ad7eded8e855c1a50af18ace2d5d7ceb78a06c
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-01-11 00:25:05 +0300

    first commit

--- SmartDayPlanner/SmartDayPlannerApp.swift
+++ SmartDayPlanner/SmartDayPlannerApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SmartDayPlannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                Text("Select an Event")
+                    .foregroundStyle(.secondary)
+            }
         }
     }
 }
