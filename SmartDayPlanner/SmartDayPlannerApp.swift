3c4c08fbb491f87b6b81c63b7519883d2cf9a133
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-01-11 00:18:44 +0300

    Initial Commit

---
+++ SmartDayPlanner/SmartDayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartDayPlannerApp.swift
+//  SmartDayPlanner
+//
+//  Created by Leonard Mutugi on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartDayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
